e3afb23ba67b45778d25f624cc65b05795eae8b4
dotdash32 <dotdash32@gmail.com>
2021-04-11 18:31:58 -0700

    homing offsets reset, doesn't move up before probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1032,7 +1032,7 @@
  * Example: `M851 Z-5` with a CLEARANCE of 4  =>  9mm from bed to nozzle.
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
-#define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
+#define Z_CLEARANCE_DEPLOY_PROBE    0 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
@@ -1131,11 +1131,11 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS MANUAL_X_HOME_POS
-#define Y_MIN_POS MANUAL_Y_HOME_POS
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS 220 // limited by tool change!
+#define Y_MAX_POS Y_BED_SIZE // limited by tool change, but maybe not?
 #define Z_MAX_POS 250
 
 /**
@@ -1377,9 +1377,9 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS -18
-#define MANUAL_Y_HOME_POS -18
-#define MANUAL_Z_HOME_POS 0
+// #define MANUAL_X_HOME_POS -18
+// #define MANUAL_Y_HOME_POS -18
+// #define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
