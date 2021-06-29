a7f3c76b8800a2cbe39a9eaeffba6e53156a90aa
Josh DeWitt <dotdash32@gmail.com>
2021-06-29 12:02:10 -0700

    fix: extruder stepper direction and abort release

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1158,7 +1158,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0.650 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0.950 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1310,7 +1310,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR true
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
