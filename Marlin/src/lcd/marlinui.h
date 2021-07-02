9850c5ec165f48c0ec9c7a90555987ff08253a7c
dotdash32 <dotdash32@gmail.com>
2021-07-01 17:32:04 -0700

    fix: double defined function?

--- Marlin/src/lcd/marlinui.h
+++ Marlin/src/lcd/marlinui.h
@@ -551,17 +551,6 @@ public:
     static inline bool use_click() { return false; }
   #endif
 
-  #if EITHER(HAS_LCD_MENU, EXTENSIBLE_UI)
-    static bool lcd_clicked;
-    static inline bool use_click() {
-      const bool click = lcd_clicked;
-      lcd_clicked = false;
-      return click;
-    }
-  #else
-    static constexpr bool lcd_clicked = false;
-    static inline bool use_click() { return false; }
-  #endif
 
   #if BOTH(HAS_LCD_MENU, ADVANCED_PAUSE_FEATURE)
     static void pause_show_message(const PauseMessage message, const PauseMode mode=PAUSE_MODE_SAME, const uint8_t extruder=active_extruder);
